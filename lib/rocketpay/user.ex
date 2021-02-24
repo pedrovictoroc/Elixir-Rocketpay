7e222d258920be8d340e58e51bd40ce96f2194d4
pedro.carvalho <pedro.carvalho@clinicasim.com>
2021-02-24 10:51:47 -0300

    changing user schema and adding Account schema

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -4,6 +4,7 @@ defmodule Rocketpay.User do
 
   import Ecto.Changeset
 
+  alias Rocketpay.Account
   alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -17,6 +18,7 @@ defmodule Rocketpay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    has_one :account, Account
 
     timestamps()
   end
